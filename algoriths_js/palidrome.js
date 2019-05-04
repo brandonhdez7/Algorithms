377aaa82e8db1b276082367457ae96712be6aabe
brandon <brandonhdez7@gmail.com>
2019-05-03 21:23:02 -0400

    title case and longest word and factorial

--- algoriths_js/palidrome.js
+++ algoriths_js/palidrome.js
@@ -9,23 +9,23 @@
 // }
 // palindrome('eyed')
 
-function palindrome(str){
+// function palindrome(str){
 
-    //find all the non-numeric characters
-   let reg = /[\W_]/g
+//     //find all the non-numeric characters
+//    let reg = /[\W_]/g
 
-   //change all characters to lower case
-   var smallStr = str.toLowerCase().replace(reg, '')
+//    //change all characters to lower case
+//    var smallStr = str.toLowerCase().replace(reg, '')
 
-    let reversed = smallStr.split("").reverse().join("")
-    console.log(reversed)
-    if (reversed === smallStr){
-        return true
-    }else{
-        return false
-    }
-}
-palindrome('eye')
+//     let reversed = smallStr.split("").reverse().join("")
+//     console.log(reversed)
+//     if (reversed === smallStr){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// palindrome('eye')
 
 
 //palindrome
@@ -45,11 +45,56 @@ function pal(str){
 console.log(pal('eye'))
 
 
+
+
+
 //find the longest word
 function findLongestWord(str){
     let words = str.split(' ')
-    console.log(words)
+    let longest = '';
+    for(let i= 0;i < words.length; i++){
+        if(words.length > longest.length)
+            longest = words;
+        // console.log(words[i])
+    }
+    // return longest.length
+    console.log(longest.length);
 }
 
-findLongestWord('The quick brown fox jumped over the lazy dog')
-// console.log(findLongestWord())
+findLongestWord('The qsuick brown fox jumped over the lazy dog')
+
+
+
+
+
+//title case
+function titleCase(str){
+    let word = str.toLowerCase().split(" ")
+    // console.log(word)
+    for(let i = 0; i < word.length; i ++){
+        word[i] = word[i][0].toUpperCase() + word[i].slice(1)
+    }
+    console.log(word.join(" "));
+}
+titleCase('i\'m a little tea pot')
+
+
+//example slice get the rest of the name staring at index 1
+let name = 'Jonathan';
+console.log(name.slice(1)) //onathan
+
+
+
+
+
+//factorialize
+//exaplain: 5*4*3*2*1 = 120;
+function factorialize(num){
+    let result = 1;
+    for(let i = 1;i <= num; i++){
+        result = result * i
+    }
+    // return results;
+    console.log(result)
+}
+factorialize(5)
